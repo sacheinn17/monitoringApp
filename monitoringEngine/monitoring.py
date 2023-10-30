06251115a3a27d1c4443b0c540477f6a95eec85d
Sachein17 <sacheinn17@gmail.com>
2023-10-30 07:42:04 +0530

    exit logic adde

--- monitoringEngine/monitoring.py
+++ monitoringEngine/monitoring.py
@@ -1,52 +1,65 @@
 import win32gui,time
 import threading
-import datetime
 from dataBase import Database
+import sys
 
 db = Database()
-
 class monitor():
     def __init__(self) -> None:
         self.app = ''
         self.context = ''
-        self.time = 0
-        self.lap = 0
-        self.lap2 = 0
-        self.date = datetime.datetime.today()
-        self.date = datetime.date(self.date.year,self.date.month,self.date.day)
-        a = threading.Thread(target = self.mointoring)
-        a.start()
+        self.time = time.time()
+        self.lap = self.time
+        self.event = threading.Event()
+        self.a = threading.Thread(target = self.mointoring)
+        self.a.start()
+
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            self.event.set()
+            time.sleep(0.2)
+            sys.exit(0)
+
+    def signal_handler(self,s,t):
+        print('You pressed Ctrl+C!')
+        self.event.set()
+        time.sleep(0.2)
+        sys.exit(0)
 
     def mointoring(self):
-        while True:
-            hwnd = win32gui.GetForegroundWindow()
-            name = win32gui.GetWindowText(hwnd)
-            new_window_name = name.split(' - ')
-            application = new_window_name[-1][0:30]
-            context = new_window_name[1 if len(new_window_name)>2 else 0][0:50]
+        while not self.event.is_set():
+                hwnd = win32gui.GetForegroundWindow()
+                name = win32gui.GetWindowText(hwnd)
+                new_window_name = name.split(' - ')
+                application = new_window_name[-1][0:30]
+                context = new_window_name[1 if len(new_window_name)>2 else 0][0:50]
 
-            if application != self.app or context != self.context and name != "Task Switching" and application != '':
-                print(new_window_name)
-                self.changed()
-                self.lap = self.time
-            
-            if self.time - self.lap >240:
-                self.changed()
+                if application != self.app or context != self.context and name != "Task Switching" and application != '':
+                    # print(new_window_name)
+                    self.changed()
+                    # print(self.time,self.lap)
+                    self.lap = self.time
+                
+                if self.time - self.lap >240:
+                    self.changed()
 
-            self.app = application
-            self.context = context
-            time.sleep(1)
-            self.time +=1
+                self.app = application
+                self.context = context
+                self.time = time.time()
+                time.sleep(1)
 
     def changed(self):
         difference = self.time-self.lap
-        # print(difference)
+        print(difference)
         if difference >3:
             # print("Entered loop")
             x,exist = db.exists(self.app,self.context)
             if exist:
-                db.updateTime(x,difference)
+                db.updateTime(x,difference/60)
             else:
-                db.addEntry(self.app,self.context,difference)
+                db.addEntry(self.app,self.context,difference/60)
 
 d = monitor()
+
