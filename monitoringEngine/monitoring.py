ceb614039b4d83c4be1007f556ea63a4ae495e36
Sachein17 <sacheinn17@gmail.com>
2023-11-03 18:07:22 +0530

    Added new logic for time keeping

--- monitoringEngine/monitoring.py
+++ monitoringEngine/monitoring.py
@@ -1,65 +1,52 @@
 import win32gui,time
 import threading
 from dataBase import Database
-import sys
 
 db = Database()
+
 class monitor():
     def __init__(self) -> None:
         self.app = ''
         self.context = ''
-        self.time = time.time()
+        self.time = int(time.time())
         self.lap = self.time
         self.event = threading.Event()
         self.a = threading.Thread(target = self.mointoring)
+        self.a.daemon = True
         self.a.start()
 
-        try:
-            while True:
-                pass
-        except KeyboardInterrupt:
-            self.event.set()
-            time.sleep(0.2)
-            sys.exit(0)
-
-    def signal_handler(self,s,t):
-        print('You pressed Ctrl+C!')
-        self.event.set()
-        time.sleep(0.2)
-        sys.exit(0)
+        while True:
+            time.sleep(10)
 
     def mointoring(self):
         while not self.event.is_set():
                 hwnd = win32gui.GetForegroundWindow()
+                # print(self.time)
                 name = win32gui.GetWindowText(hwnd)
                 new_window_name = name.split(' - ')
                 application = new_window_name[-1][0:30]
                 context = new_window_name[1 if len(new_window_name)>2 else 0][0:50]
 
                 if application != self.app or context != self.context and name != "Task Switching" and application != '':
-                    # print(new_window_name)
                     self.changed()
-                    # print(self.time,self.lap)
-                    self.lap = self.time
                 
-                if self.time - self.lap >240:
+                if self.time - self.lap >300:
                     self.changed()
 
                 self.app = application
                 self.context = context
-                self.time = time.time()
+                self.time = int(time.time())
                 time.sleep(1)
 
     def changed(self):
         difference = self.time-self.lap
-        print(difference)
+        self.lap = self.time
+        print(self.app,self.context, self.time)
         if difference >3:
-            # print("Entered loop")
             x,exist = db.exists(self.app,self.context)
             if exist:
-                db.updateTime(x,difference/60)
+                db.updateTime(x,difference)
             else:
-                db.addEntry(self.app,self.context,difference/60)
+                db.addEntry(self.app,self.context,difference)
 
 d = monitor()
-
